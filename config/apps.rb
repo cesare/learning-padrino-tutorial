1c618f120763caefacfa3ac489272b0533e0b1c9
SAWADA Tadashi <cesare@mayverse.jp>
2013-03-20 16:55:36 +0900

    Add sub app /api
    $ padrino g app api

--- config/apps.rb
+++ config/apps.rb
@@ -36,3 +36,4 @@ end
 Padrino.mount('SampleBlog::App', :app_file => Padrino.root('app/app.rb')).to('/')
 
 Padrino.mount("SampleBlog::Admin", :app_file => File.expand_path('../../admin/app.rb', __FILE__)).to("/admin")
+Padrino.mount('Api::App', :app_file => Padrino.root('api/app.rb')).to('/api')
