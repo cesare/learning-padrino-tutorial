c315f510d3aa29f273187ce67e2aba28c963c9fc
SAWADA Tadashi <cesare@mayverse.jp>
2013-03-20 15:01:07 +0900

    Add index and show action

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,7 +1,11 @@
 SampleBlog::App.controllers :posts do
   get :index do
+    @posts = Post.all(order: 'created_at DESC')
+    render 'posts/index'
   end
 
   get :show do
+    @post = Post.find_by_id(params[:id])
+    render 'posts/show'
   end
 end
